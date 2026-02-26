f42b0d27bc54b3fd453f963ceb4752d6c8793ce8
lalitchaudharry <lc7906798@gmail.com>
2026-02-26 11:49:52 +0530

    first commit

---
+++ src/main/java/com/springbites/SpringbitesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.springbites;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbitesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbitesBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springbites/SpringbitesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbites;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbitesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
